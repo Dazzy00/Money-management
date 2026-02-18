ebfdb122d1aed7d3a46c1019df82c7d31344208b
frappe <dazzyjohn1@gmail.com>
2026-02-18 14:47:16 +0000

    Loan

--- money_management/money_management/doctype/library_member/library_member.js
+++ money_management/money_management/doctype/library_member/library_member.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2026, Dazzy and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Member", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Library Member", {
+	refresh(frm) {
+        if (frm.doc.first_name) {
+            frappe.msgprint("Habari yako, " + frm.doc.first_name);
+        }
+	},
+    first_name(frm) {
+        first_name = frm.doc.first_name;
+        last_name = frm.doc.last_name;
+        full_name = first_name + " " + last_name;
+        frm.set_value("full_name", full_name);
+    },
+    last_name(frm) {
+        first_name = frm.doc.first_name;
+        last_name = frm.doc.last_name;
+        full_name = first_name + " " + last_name;
+        frm.set_value("full_name", full_name);
+    },
+    after_save(frm) {
+        frappe.msgprint("Full Name: " + frm.doc.full_name);
+    },
+});
--- money_management/money_management/doctype/loan/loan.js
+++ money_management/money_management/doctype/loan/loan.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2026, Dazzy and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Loan", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Loan", {
+	refresh(frm) {
+        frm.set_query("book", function(){
+            return{
+                filters:{status:"Available"}
+            }
+        })
+	},
+});
